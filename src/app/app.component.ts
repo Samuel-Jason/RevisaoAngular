3abbaef0ead98cd5cfff48659940fbcaa5249de2
Samuel <samuelljason@gmail.com>
2024-06-25 21:03:38 -0300

    Implementacao inicial do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhotoComponent } from "./photos/photo/photo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, PhotoComponent]
 })
 export class AppComponent {
-  title = 'proj';
+  photos = [{
+    url: 'http://assets.jpg',
+    description: 'leao'
+  },
+  {
+    url: 'http://assets.jpg',
+    description: 'leao'
+  }]
 }
---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo.component.html',
+  styleUrl: './photo.component.css'
+})
+export class PhotoComponent {
+
+  @Input() description = '';
+  @Input() url = '';
+}
