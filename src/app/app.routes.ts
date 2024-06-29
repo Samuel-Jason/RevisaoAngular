26a3cc8084e6d304ff11c5db6b06efe6a47ebacd
Samuel <samuelljason@gmail.com>
2024-06-28 21:48:02 -0300

    Adicionado Rotas e um componente para quando acessar qualquer pagina inexistente pela url mostrar uma tela de erro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PhotoComponent } from './photos/photo/photo.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NgModel } from '@angular/forms';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user/flavio', component:PhotoComponent},
+  { path: 'p/add', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent}
+];
